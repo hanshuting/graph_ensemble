0b1196146bf27990fb31165a311456d25b496ed3
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-04-08 23:13:48 -0400

    save_best_model saves more parameters
    Added the best model's `s_lambda`, `p_lambda`, and `density`.

--- scripts/graphs/save_best_model.m
+++ scripts/graphs/save_best_model.m
@@ -10,5 +10,9 @@ edge_pot = best_model.theta.edge_potentials;
 G = best_model.theta.G;
 F = best_model.theta.F;
 logZ = best_model.theta.logZ;
+s_lambda = best_model.s_lambda;
+p_lambda = best_model.p_lambda;
+density = best_model.density;
 
-save([savepath savename '_best_model_full.mat'],'graph','node_pot','edge_pot','G','F','logZ','-v7.3');
+save([savepath savename '_best_model_full.mat'], 'graph', 'node_pot', 'edge_pot', ...
+    'G', 'F', 'logZ', 's_lambda', 'p_lambda', 'density', '-v7.3');
