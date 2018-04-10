d58579c9367a3f81c348be03e63a80055ad485b2
hst <shuting.han1@gmail.com>
2018-04-10 15:33:12 -0400

    removed files in .gitignore

--- fwMatch-darpa/mvc/scripts/setBoxStyle.m
+++
@@ -1,8 +0,0 @@
-function [] = setBoxStyle(h,linew)
-
-set(h,'linewidth',linew);
-set(h(7,:),'visible','off');
-set(h(6,:),'linewidth',2*linew);
-set(findobj(gcf,'-regexp','Tag','\w*Whisker'),'LineStyle','-')
-
-end
