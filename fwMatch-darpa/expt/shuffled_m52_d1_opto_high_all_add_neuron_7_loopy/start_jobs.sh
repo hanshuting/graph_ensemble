970c42c94848f9ff88665a6b056eb61ac14db01f
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-04-27 10:32:13 -0400

    Folder prepped with Shuting's specified params

--- fwMatch-darpa/expt/shuffled_m52_d1_opto_high_all_add_neuron_7_loopy/start_jobs.sh
+++
@@ -1,6 +0,0 @@
-#If you are having problem with line endings use ":set ff=unix" in vim
-
-rm -f ./results/result*.mat
-rm -f ./yeti_logs/*
-rm -f ./job_logs/*
-cd ../.. && qsub expt/shuffled_m52_d1_opto_high_all_add_neuron_7_loopy/yeti_config.sh
