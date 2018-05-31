2145effcb16a4d1b77adb8eee489d13f466c2c32
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-05-31 11:42:00 -0400

    Remove old expt folders, clean up current

--- fwMatch-darpa/expt/m21_d2_vis_all_high_add_neuron_loopy/start_jobs.sh
+++
@@ -1,6 +0,0 @@
-#If you are having problem with line endings use ":set ff=unix" in vim
-
-rm -f ./results/result*.mat
-rm -f ./yeti_logs/*
-rm -f ./job_logs/*
-cd ../.. && qsub expt/m21_d2_vis_all_high_add_neuron_loopy/yeti_config.sh
