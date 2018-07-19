54b9dad53f56ebffa2216e5b8939db3644752ee0
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-07-19 15:53:40 -0400

    Keep all user editable parameters at top of run_full_temporal.py

--- fwMatch-darpa/expt/run_full_temporal.py
+++ fwMatch-darpa/expt/run_full_temporal.py
@@ -12,6 +12,19 @@ EXPT_NAME = "temporal"
 DATA_DIR = "~/data/"
 USER = "jds2270"
 EMAIL = "jds2270@columbia.edu"
+S_LAMBDAS = {'parallize': True,      # If True, each point generates a distinct config and job
+             'num_points': 3,        # Number of parameter values to test between min and max
+             'min': 2e-03,
+             'max': 5e-01}
+DENSITIES = {'parallize': False,      # If True, each point generates a distinct config and job
+             'num_points': 3,        # Number of parameter values to test between min and max
+             'min': 0.1,
+             'max': 0.3}
+P_LAMBDAS = {'parallize': True,      # If True, each point generates a distinct config and job
+             'num_points': 2,        # Number of parameter values to test between min and max
+             'min': 1e+01,
+             'max': 1e+04}
+TIME_SPAN = 2
 # *** END USER EDITABLE VARIABLES ***
 
 # *** start constants ***
@@ -51,19 +64,25 @@ def setup_exec_train_model(condition_names):
             f.write("    'yeti_user', '{}', ...\n".format(USER))
             f.write("    'compute_true_logZ', false, ...\n")
             f.write("    'reweight_denominator', 'mean_degree', ...\n")
-            f.write("    's_lambda_splits', 3, ...\n")
-            f.write("    's_lambdas_per_split', 1, ...\n")
-            f.write("    's_lambda_min', 2e-03, ...\n")
-            f.write("    's_lambda_max', 5e-01, ...\n")
-            f.write("    'density_splits', 1, ...\n")
-            f.write("    'densities_per_split', 6, ...\n")
-            f.write("    'density_min', 0.1, ...\n")
-            f.write("    'density_max', 0.3, ...\n")
-            f.write("    'p_lambda_splits', 2, ...\n")
-            f.write("    'p_lambdas_per_split', 1, ...\n")
-            f.write("    'p_lambda_min', 1e+01, ...\n")
-            f.write("    'p_lambda_max', 1e+04, ...\n")
-            f.write("    'time_span', 2);\n")
+            f.write("    's_lambda_splits', {}, ...\n".format(
+                S_LAMBDAS['num_points'] if S_LAMBDAS['parallize'] else 1))
+            f.write("    's_lambdas_per_split', {}, ...\n".format(
+                1 if S_LAMBDAS['parallize'] else S_LAMBDAS['num_points']))
+            f.write("    's_lambda_min', {}, ...\n".format(S_LAMBDAS['min']))
+            f.write("    's_lambda_max', {}, ...\n".format(S_LAMBDAS['max']))
+            f.write("    'density_splits', {}, ...\n".format(
+                DENSITIES['num_points'] if DENSITIES['parallize'] else 1))
+            f.write("    'densities_per_split', {}, ...\n".format(
+                1 if DENSITIES['parallize'] else DENSITIES['num_points']))
+            f.write("    'density_min', {}, ...\n".format(DENSITIES['min']))
+            f.write("    'density_max', {}, ...\n".format(DENSITIES['max']))
+            f.write("    'p_lambda_splits', {}, ...\n".format(
+                P_LAMBDAS['num_points'] if P_LAMBDAS['parallize'] else 1))
+            f.write("    'p_lambdas_per_split', {}, ...\n".format(
+                1 if P_LAMBDAS['parallize'] else P_LAMBDAS['num_points']))
+            f.write("    'p_lambda_min', {}, ...\n".format(P_LAMBDAS['min']))
+            f.write("    'p_lambda_max', {}, ...\n".format(P_LAMBDAS['max']))
+            f.write("    'time_span', {});\n".format(TIME_SPAN))
         f.closed
         print("done writing write_configs_for_loopy.m")
 
