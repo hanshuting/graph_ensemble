e8ca038cea5d7d1d842246805a0f5cbe9e435f27
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-07-20 11:50:22 -0400

    create_shuffle-config_files - remove deprecated addParamValue calls
    Renamed from create_config_files, which is a distinct function for the training step.

--- fwMatch-darpa/expt/shuffled_template/create_shuffle_config_files.m
+++ fwMatch-darpa/expt/shuffled_template/create_shuffle_config_files.m
@@ -10,42 +10,44 @@ function create_config_files(varargin)
 
     parser = inputParser;
 
-    parser.addParamValue('training_test_split', .8, @isscalar);
-    parser.addParamValue('BCFW_max_iterations', 75000, @isscalar);
-    parser.addParamValue('structure_type', 'loopy', @ischar);
-    parser.addParamValue('experiment_name', 'spikes_opto_on', @ischar);
-    parser.addParamValue('email_for_notifications', 'jds2270@columbia.edu', @ischar);
-    parser.addParamValue('yeti_user', 'jds2270', @ischar);
-    parser.addParamValue('compute_true_logZ', false, @islogical);
-    parser.addParamValue('reweight_denominator', 'max_degree');
+    parser.addParameter('experiment_name', 'spikes_opto_on', @ischar);
+    parser.addParameter('email_for_notifications', 'jds2270@columbia.edu', @ischar);
+    parser.addParameter('yeti_user', 'jds2270', @ischar);
 
-    parser.addParamValue('s_lambda_splits', 1, @isscalar);
-    parser.addParamValue('s_lambdas_per_split', 1, @isscalar);
-    parser.addParamValue('s_lambda_min', 1e-01, @isscalar);
-    parser.addParamValue('s_lambda_max', 1e-01, @isscalar);
+    parser.addParameter('training_test_split', .8, @isscalar);
+    parser.addParameter('BCFW_max_iterations', 75000, @isscalar);
+    parser.addParameter('structure_type', 'loopy', @ischar);
+    parser.addParameter('compute_true_logZ', false, @islogical);
+    parser.addParameter('reweight_denominator', 'max_degree');
 
-    parser.addParamValue('density_splits', 1, @isscalar);
-    parser.addParamValue('densities_per_split', 1, @isscalar);
-    parser.addParamValue('density_min', 0.05, @isscalar);
-    parser.addParamValue('density_max', 0.05, @isscalar);
+    parser.addParameter('s_lambda_splits', 1, @isscalar);
+    parser.addParameter('s_lambdas_per_split', 1, @isscalar);
+    parser.addParameter('s_lambda_min', 1e-01, @isscalar);
+    parser.addParameter('s_lambda_max', 1e-01, @isscalar);
 
-    parser.addParamValue('p_lambda_splits', 1, @isscalar);
-    parser.addParamValue('p_lambdas_per_split', 1, @isscalar);
-    parser.addParamValue('p_lambda_min', 1e+01, @isscalar);
-    parser.addParamValue('p_lambda_max', 1e+01, @isscalar);
+    parser.addParameter('density_splits', 1, @isscalar);
+    parser.addParameter('densities_per_split', 1, @isscalar);
+    parser.addParameter('density_min', 0.05, @isscalar);
+    parser.addParameter('density_max', 0.05, @isscalar);
+
+    parser.addParameter('p_lambda_splits', 1, @isscalar);
+    parser.addParameter('p_lambdas_per_split', 1, @isscalar);
+    parser.addParameter('p_lambda_min', 1e+01, @isscalar);
+    parser.addParameter('p_lambda_max', 1e+01, @isscalar);
 
     parser.addParameter('time_span', 1, @isscalar);
 
-    parser.addParamValue('num_shuffle', 100, @isscalar);
+    parser.addParameter('num_shuffle', 100, @isscalar);
 
     parser.parse(varargin{:})
+
+    experiment_name = parser.Results.experiment_name;
+    email_for_notifications = parser.Results.email_for_notifications;
+    yeti_user = parser.Results.yeti_user;
 
     training_test_split = parser.Results.training_test_split;
     BCFW_max_iterations = parser.Results.BCFW_max_iterations;
     structure_type = parser.Results.structure_type;
-    experiment_name = parser.Results.experiment_name;
-    email_for_notifications = parser.Results.email_for_notifications;
-    yeti_user = parser.Results.yeti_user;
     compute_true_logZ = parser.Results.compute_true_logZ;
     if (compute_true_logZ); compute_true_logZ_str='true'; else;  compute_true_logZ_str='false'; end
     reweight_denominator = parser.Results.reweight_denominator;
