cd8892a3cc35b5df6a70fd17ba80ce741127d69a
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-07-26 12:05:48 -0400

    Update merge_all_results
    Use matlab function fullfile to construct filepaths.
    Eliminates a point of failure: no problem if the path passed does not have a trailing /.

--- fwMatch-darpa/src/expt_framework/merge_all_results.m
+++ fwMatch-darpa/src/expt_framework/merge_all_results.m
@@ -3,15 +3,15 @@ function [model_collection] = merge_all_results(results_folder_path)
 %
 % Input
 %   results_folder_path (Optional): String to folder containing result*.mat
-%       files to merge. MUST include a trailing /.
+%       files to merge.
     if nargin < 1
         results_folder_path = pwd;
     end
 
     % iterate through all result files merging the model collections
-    result_files = dir(sprintf('%sresult*.mat', results_folder_path));
+    result_files = dir(fullfile(results_folder_path, 'result*.mat'));
     for i = 1:length(result_files)
-        split_result = load(sprintf('%s%s', results_folder_path, result_files(i).name), 'model_collection');
+        split_result = load(fullfile(results_folder_path, result_files(i).name), 'model_collection');
         if i == 1
             merged_model_collection = split_result.model_collection;
         else
@@ -20,5 +20,5 @@ function [model_collection] = merge_all_results(results_folder_path)
     end
     model_collection = merged_model_collection;
 
-    save(sprintf('%smodel_collection.mat', results_folder_path), 'model_collection', '-v7.3');
+    save(fullfile(results_folder_path, 'model_collection.mat'), 'model_collection', '-v7.3');
 end
