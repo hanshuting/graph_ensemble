fafc2ad0b55a40f7728f4449720ef46504729621
hanshuting <shuting.han@columbia.edu>
2018-09-12 05:59:08 -0400

    remove fwMatch/run

--- fwMatch-darpa/run/old/runLearnBethe.m
+++
@@ -1,7 +0,0 @@
-%% Example for learnBethe.
-% You should see obj converge to the negative maximum likelihood and dMu
-% converge to zero.
-
-Areasonable = rand(5,5);
-Mreasonable = sample_perms(Areasonable, 10000);
-[thetaReasonable, histReasonable] = learnBethe(Mreasonable, @minBethe, 'thetaTrue', Areasonable);
--- fwMatch-darpa/run/old/testRankPerm.m
+++
@@ -1,9 +0,0 @@
-%% Blah
-D = 6;
-M = factorial(D);
-
-for m = 1:M
-    p = oneperm(D, m);
-    m2 = rankperm(p);
-    assert(m == m2, 'Inversion fail');
-end
