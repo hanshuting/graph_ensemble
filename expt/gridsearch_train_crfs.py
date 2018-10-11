e00c1b7927705c44617f509f03f856e2b26c2312
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-10-10 21:04:22 -0400

    gridsearch_train_crfs objectified
    MODEL_TYPE and PARAMS_TO_EXTRACT moved into class def.

--- expt/gridsearch_train_crfs.py
+++ expt/gridsearch_train_crfs.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""Expected to be run from the expt folder.
+"""Class to gridsearch parameters for training a graphical model on neuronal
+    spiketrain data.
 """
 import time
 import sys
 import os
-import crf_util
-import yeti_support
-
 import logging
 
-# *** start constants ***
-MODEL_TYPE = "loopy"
-
-# These parameters and their order must match best_parameters.txt.
-# See save_best_parameters.m for best_parameters.txt creation.
-PARAMS_TO_EXTRACT = ["s_lambda", "density", "p_lambda", "time_span"]
-# *** end constants ***
-
-
-def start_logfile(debug_filelogging, expt_dir, experiment, **_):
-    expt_dir = os.path.expanduser(expt_dir)
-    log_fname = os.path.join(expt_dir, experiment, "gridsearch_train_crfs.log")
-    logfile_handler = crf_util.get_FileHandler(
-        log_fname, debug_filelogging=debug_filelogging
-    )
-    logger.addHandler(logfile_handler)
-    logger.debug("Logging file handler to {} added.".format(log_fname))
+import crf_util
+import yeti_support
 
 
 class GridsearchTrial(object):
     """docstring for GridsearchTrial"""
+
+    MODEL_TYPE = "loopy"
+    # These parameters and their order must match best_parameters.txt.
+    # See save_best_parameters.m for best_parameters.txt creation.
+    PARAMS_TO_EXTRACT = ["s_lambda", "density", "p_lambda", "time_span"]
 
     def __init__(
         self,
@@ -75,180 +63,177 @@ class GridsearchTrial(object):
         self.time_span = params["time_span"]
         self.edges = params["edges"]
         self.no_same_neuron_edges = params["no_same_neuron_edges"]
-        self.data_file = "{}_{}.mat".format(params["experiment_name"], condition)
+        self.data_file = "{}_{}.mat".format(self.experiment_group, self.condition_name)
         self.experiment = "{}_{}_{}".format(
-            params["experiment_name"], condition, MODEL_TYPE
+            self.experiment_group, self.condition_name, self.MODEL_TYPE
         )
-        self.expt_dir = os.path.join(params["source_directory"], "expt")
+        self.expt_dir = os.path.join(self.source_dir, "expt")
 
-        self.start_jobs = start_gridsearch_jobs
-        self.test_gs_get_best_params = test_train_CRFs
+        self.start_jobs = self.start_gridsearch_jobs
+        self.test_gs_get_best_params = self.test_train_CRFs
         # Update settings for cluster specified, if any
         if self.cluster_architecture == "yeti":
             self.logger.info("Yeti cluster architecture selected for gridsearch.")
             yeti_support.get_yeti_gs_metadata(self, parser=parameters_parser)
 
+    def _start_logfile(self):
+        # TODO: Update to working_dir
+        expt_dir = os.path.expanduser(self.expt_dir)
+        log_fname = os.path.join(expt_dir, self.experiment, "gridsearch_train_crfs.log")
+        logfile_handler = crf_util.get_FileHandler(
+            log_fname, debug_filelogging=self.debug_filelogging
+        )
+        self.logger.addHandler(logfile_handler)
+        self.logger.debug("Logging file handler to {} added.".format(log_fname))
 
-def create_working_dir(params):
-    logger.info("Creating working directory: {}".format(params["experiment"]))
-    os.makedirs(os.path.expanduser(params["experiment"]))
+    def _create_working_dir(self):
+        # TODO: Use .destination_path
+        self.working_dir = self.experiment
+        self.logger.info("Creating working directory: {}".format(self.working_dir))
+        os.makedirs(os.path.expanduser(self.working_dir))
 
-
-def create_write_configs_for_loopy_m(params):
-    fname = os.path.join(params["experiment"], "write_configs_for_loopy.m")
-    # TODO: Just call create_configs directly
-    with open(fname, "w") as f:
-        f.write("create_configs( ...\n")
-        f.write(
-            "    'datapath', '{}{}', ...\n".format(
-                params["data_directory"], params["data_file"]
+    def _create_write_configs_for_loopy_m(self):
+        # TODO: Fix fname path
+        fname = os.path.join(self.experiment, "write_configs_for_loopy.m")
+        # TODO: Just call create_configs directly
+        with open(fname, "w") as f:
+            f.write("create_configs( ...\n")
+            f.write("    'datapath', '{}{}', ...\n".format(self.data_dir, self.data_file))
+            f.write("    'experiment_name', '{}', ...\n".format(self.experiment))
+            try:
+                f.write("    'email_for_notifications', '{}', ...\n".format(self.email))
+            except AttributeError:
+                self.logger.debug("No notifications email setting provided. Skipping.")
+            try:
+                f.write("    'yeti_user', '{}', ...\n".format(self.username))
+            except AttributeError:
+                self.logger.debug("No yeti username provided. Skipping.")
+            f.write("    'compute_true_logZ', false, ...\n")
+            f.write("    'reweight_denominator', 'mean_degree', ...\n")
+            s_lambdas = self.S_LAMBDAS
+            f.write(
+                "    's_lambda_splits', {}, ...\n".format(
+                    s_lambdas["num_points"] if s_lambdas["parallize"] else 1
+                )
             )
-        )
-        f.write("    'experiment_name', '{}', ...\n".format(params["experiment"]))
-        try:
-            f.write("    'email_for_notifications', '{}', ...\n".format(params["email"]))
-        except KeyError:
-            logger.debug("No notifications email setting provided. Skipping.")
-        try:
-            f.write("    'yeti_user', '{}', ...\n".format(params["username"]))
-        except KeyError:
-            logger.debug("No yeti username provided. Skipping.")
-        f.write("    'compute_true_logZ', false, ...\n")
-        f.write("    'reweight_denominator', 'mean_degree', ...\n")
-        s_lambdas = params["S_LAMBDAS"]
-        f.write(
-            "    's_lambda_splits', {}, ...\n".format(
-                s_lambdas["num_points"] if s_lambdas["parallize"] else 1
+            f.write(
+                "    's_lambdas_per_split', {}, ...\n".format(
+                    1 if s_lambdas["parallize"] else s_lambdas["num_points"]
+                )
             )
-        )
-        f.write(
-            "    's_lambdas_per_split', {}, ...\n".format(
-                1 if s_lambdas["parallize"] else s_lambdas["num_points"]
+            f.write("    's_lambda_min', {}, ...\n".format(s_lambdas["min"]))
+            f.write("    's_lambda_max', {}, ...\n".format(s_lambdas["max"]))
+            densities = self.DENSITIES
+            f.write(
+                "    'density_splits', {}, ...\n".format(
+                    densities["num_points"] if densities["parallize"] else 1
+                )
             )
-        )
-        f.write("    's_lambda_min', {}, ...\n".format(s_lambdas["min"]))
-        f.write("    's_lambda_max', {}, ...\n".format(s_lambdas["max"]))
-        densities = params["DENSITIES"]
-        f.write(
-            "    'density_splits', {}, ...\n".format(
-                densities["num_points"] if densities["parallize"] else 1
+            f.write(
+                "    'densities_per_split', {}, ...\n".format(
+                    1 if densities["parallize"] else densities["num_points"]
+                )
             )
-        )
-        f.write(
-            "    'densities_per_split', {}, ...\n".format(
-                1 if densities["parallize"] else densities["num_points"]
+            f.write("    'density_min', {}, ...\n".format(densities["min"]))
+            f.write("    'density_max', {}, ...\n".format(densities["max"]))
+            p_lambdas = self.P_LAMBDAS
+            f.write(
+                "    'p_lambda_splits', {}, ...\n".format(
+                    p_lambdas["num_points"] if p_lambdas["parallize"] else 1
+                )
             )
-        )
-        f.write("    'density_min', {}, ...\n".format(densities["min"]))
-        f.write("    'density_max', {}, ...\n".format(densities["max"]))
-        p_lambdas = params["P_LAMBDAS"]
-        f.write(
-            "    'p_lambda_splits', {}, ...\n".format(
-                p_lambdas["num_points"] if p_lambdas["parallize"] else 1
+            f.write(
+                "    'p_lambdas_per_split', {}, ...\n".format(
+                    1 if p_lambdas["parallize"] else p_lambdas["num_points"]
+                )
             )
-        )
-        f.write(
-            "    'p_lambdas_per_split', {}, ...\n".format(
-                1 if p_lambdas["parallize"] else p_lambdas["num_points"]
+            f.write("    'p_lambda_min', {}, ...\n".format(p_lambdas["min"]))
+            f.write("    'p_lambda_max', {}, ...\n".format(p_lambdas["max"]))
+            f.write("    'edges', '{}', ...\n".format(self.edges.lower()))
+            f.write(
+                "    'no_same_neuron_edges', {}, ...\n".format(
+                    str(self.no_same_neuron_edges).lower()
+                )
             )
-        )
-        f.write("    'p_lambda_min', {}, ...\n".format(p_lambdas["min"]))
-        f.write("    'p_lambda_max', {}, ...\n".format(p_lambdas["max"]))
-        f.write("    'edges', '{}', ...\n".format(params["edges"].lower()))
-        f.write(
-            "    'no_same_neuron_edges', {}, ...\n".format(
-                str(params["no_same_neuron_edges"]).lower()
+            f.write("    'time_span', {});\n".format(self.time_span))
+        f.closed
+        self.logger.info("done writing {}".format(fname))
+
+    def start_gridsearch_jobs(self):
+        if (
+            self.S_LAMBDAS["parallize"]
+            or self.DENSITIES["parallize"]
+            or self.P_LAMBDAS["parallize"]
+        ):
+            self.logger.warning(
+                "WARNING: ONLY config1.m WILL BE USED. "
+                + "Cluster architecture {} ".format(self.cluster_architecture)
+                + "has no parallel execution capability, "
+                + "but found parallize options set to True."
             )
+        curr_dir = os.getcwd()
+        self.logger.debug("curr_dir = {}.".format(curr_dir))
+        os.chdir(self.source_dir)
+        self.logger.debug("changed into dir: {}".format(os.getcwd()))
+        shell_cmd = ".{}run.sh {} 1".format(os.sep, self.experiment)
+        crf_util.run_command(shell_cmd, shell=True)
+        os.chdir(curr_dir)
+        self.logger.debug("changed back to dir: {}".format(os.getcwd()))
+
+    def setup_exec_train_model(self):
+        """Mostly follows old create_script.pl.
+
+            Expect to be in expt folder at start.
+
+        Args:
+            params (dict): Model parameters.
+        """
+        self._create_working_dir()
+        self._start_logfile()
+        self._create_write_configs_for_loopy_m()
+
+        # Move into experiment folder
+        curr_dir = os.getcwd()
+        self.logger.debug("curr_dir = {}.".format(curr_dir))
+        os.chdir(self.experiment)
+        self.logger.debug("changed into dir: {}".format(os.getcwd()))
+        crf_util.run_matlab_command(
+            "write_configs_for_{},".format(self.MODEL_TYPE), add_path=self.source_dir
         )
-        f.write("    'time_span', {});\n".format(params["time_span"]))
-    f.closed
-    logger.info("done writing {}".format(fname))
+        self.logger.info("\nTraining configs generated.")
+        # start_gridsearch_jobs by default
+        self.start_jobs()
 
+        os.chdir(curr_dir)
+        self.logger.debug("changed back to dir: {}".format(os.getcwd()))
 
-def start_gridsearch_jobs(params):
-    if (
-        params["S_LAMBDAS"]["parallize"]
-        or params["DENSITIES"]["parallize"]
-        or params["P_LAMBDAS"]["parallize"]
-    ):
-        logger.warning(
-            "WARNING: ONLY config1.m WILL BE USED. "
-            + "Cluster architecture {} ".format(params["cluster_architecture"])
-            + "has no parallel execution capability, "
-            + "but found parallize options set to True."
+    def merge_save_train_models(self):
+        results_path = os.path.join(self.experiment, "results")
+        return crf_util.run_matlab_command(
+            "save_best_params('{}'); ".format(results_path), add_path=self.source_dir
         )
-    curr_dir = os.getcwd()
-    logger.debug("curr_dir = {}.".format(curr_dir))
-    os.chdir(params["source_directory"])
-    logger.debug("changed into dir: {}".format(os.getcwd()))
-    shell_cmd = ".{}run.sh {} 1".format(os.sep, params["experiment"])
-    crf_util.run_command(shell_cmd, shell=True)
-    os.chdir(curr_dir)
-    logger.debug("changed back to dir: {}".format(os.getcwd()))
 
+    def get_best_parameters(self):
+        """Get the best parameters from gridsearch results.
 
-def setup_exec_train_model(params):
-    """Mostly follows old create_script.pl.
+        Returns:
+            dict: Best parameters from the gridsearch. Parameters stored as a dict with
+                PARAMS_TO_EXTRACT as the keys.
+        """
+        best_params = {}
+        # grab and return best params
+        results_path = os.path.join(self.experiment, "results")
+        with open(os.path.join(results_path, "best_parameters.txt"), "r") as f:
+            for param in self.PARAMS_TO_EXTRACT:
+                best_params[param] = float(f.readline())
+        f.closed
 
-        Expect to be in expt folder at start.
+        return best_params
 
-    Args:
-        params (dict): Model parameters.
-    """
-    create_working_dir(params)
-    start_logfile(**params)
-    create_write_configs_for_loopy_m(params)
-
-    # Move into experiment folder
-    curr_dir = os.getcwd()
-    logger.debug("curr_dir = {}.".format(curr_dir))
-    os.chdir(params["experiment"])
-    logger.debug("changed into dir: {}".format(os.getcwd()))
-    crf_util.run_matlab_command(
-        "write_configs_for_{},".format(MODEL_TYPE), add_path=params["source_directory"]
-    )
-    logger.info("\nTraining configs generated.")
-
-    # start_gridsearch_jobs by default
-    params["start_jobs"](params)
-
-    os.chdir(curr_dir)
-    logger.debug("changed back to dir: {}".format(os.getcwd()))
-
-
-def merge_save_train_models(experiment, source_directory, **kwargs):
-    results_path = os.path.join(experiment, "results")
-    return crf_util.run_matlab_command(
-        "save_best_params('{}'); ".format(results_path), add_path=source_directory
-    )
-
-
-def get_best_parameters(experiment, **kwargs):
-    """Get the best parameters from gridsearch results.
-
-    Args:
-        experiment (str): Path to working directory containing results subdirectory.
-
-    Returns:
-        dict: Best parameters from the gridsearch. Parameters stored as a dict with
-            PARAMS_TO_EXTRACT as the keys.
-    """
-    best_params = {}
-
-    # grab and return best params
-    results_path = os.path.join(experiment, "results")
-    with open(os.path.join(results_path, "best_parameters.txt"), "r") as f:
-        for param in PARAMS_TO_EXTRACT:
-            best_params[param] = float(f.readline())
-    f.closed
-
-    return best_params
-
-
-def test_train_CRFs(experiment, **kwargs):
-    filebase = os.path.join(experiment, "results", "result")
-    num_jobs = 1
-    return crf_util.get_max_job_done(filebase) >= num_jobs
+    def test_train_CRFs(self):
+        filebase = os.path.join(self.experiment, "results", "result")
+        num_jobs = 1
+        return crf_util.get_max_job_done(filebase) >= num_jobs
 
 
 def main(condition, ini_fname="crf_parameters.ini"):
@@ -258,30 +243,29 @@ def main(condition, ini_fname="crf_parameters.ini"):
         condition (str): Condition name.
         ini_fname (str, optional): Filepath of settings file to read.
     """
-    params = get_conditions_metadata(condition, ini_fname)
+    gridsearch = GridsearchTrial(condition, ini_fname=ini_fname)
 
     # Update logging if module is invoked from the command line
     if __name__ == "__main__":
         # Assume top log position
-        logger = logging.getLogger("top")
-        logger.setLevel(logging.DEBUG)
+        gridsearch.logger = logging.getLogger("top")
+        gridsearch.logger.setLevel(logging.DEBUG)
         # Create stdout log handler
-        verbosity = params["verbosity"]
-        logger.addHandler(crf_util.get_StreamHandler(verbosity))
-        logger.debug("Logging stream handler to sys.stdout added.")
+        gridsearch.logger.addHandler(crf_util.get_StreamHandler(gridsearch.verbosity))
+        gridsearch.logger.debug("Logging stream handler to sys.stdout added.")
 
-    setup_exec_train_model(params)
+    gridsearch.setup_exec_train_model()
     # Wait for train CRF to be done
     # Run merge and save_best
-    params["to_test"] = params["test_gs_get_best_params"]
-    params["to_run"] = merge_save_train_models
-    crf_util.wait_and_run(params)
-    best_params_path = os.path.join(
-        params["expt_dir"], params["experiment"], "results", "best_parameters.txt"
+    crf_util.wait_and_run(
+        gridsearch.test_gs_get_best_params, gridsearch.merge_save_train_models
     )
-    logger.info(
+    best_params_path = os.path.join(
+        gridsearch.expt_dir, gridsearch.experiment, "results", "best_parameters.txt"
+    )
+    gridsearch.logger.info(
         "Grid search complete. Best parameters in {}".format(best_params_path)
-        + " in the following order:\n{}\n".format(PARAMS_TO_EXTRACT)
+        + " in the following order:\n{}\n".format(gridsearch.PARAMS_TO_EXTRACT)
     )
 
 
