9199b6b74fc2e0da4d94172c9e69af0b59cf585a
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-10-17 09:28:11 -0400

    shuffled_control_crfs objectified and set as subclass of Workflow

--- expt/shuffled_control_crfs.py
+++ expt/shuffled_control_crfs.py
@@ -5,15 +5,15 @@
 import time
 import sys
 import os
-
-import crf_util
-import gridsearch_train_crfs
-import yeti_support
-
 import logging
 
+import crf_util
+from gridsearch_train_crfs import GridsearchTrial
+import yeti_support
+from workflow import Workflow
 
-class ShuffledControlsTrial(object):
+
+class ShuffledControlsTrial(Workflow):
     """docstring for ShuffledControlsTrial
 
     Attributes:
@@ -32,41 +32,35 @@ class ShuffledControlsTrial(object):
         destination_path=None,
         logger=None,
     ):
+        if logger is None:
+            logger = logging.getLogger("top." + __name__)
+            logger.setLevel(logging.DEBUG)
         super().__init__(condition_name, ini_fname, destination_path, logger)
-        # self.condition_name = condition_name
-        # self.ini_fname = ini_fname
-        # if logger is None:
-        #     logger = logging.getLogger("top." + __name__)
-        #     logger.setLevel(logging.DEBUG)
-        # self.logger = logger
 
-        parameters_parser = crf_util.get_raw_configparser(fname=self.ini_fname)
-        params = crf_util.get_GeneralOptions(parser=parameters_parser)
-        self.experiment_group = params["experiment_name"]
-        self.data_dir = params["data_directory"]
-        self.source_dir = params["source_directory"]
-        self.verbosity = params["verbosity"]
-        self.debug_filelogging = params["debug_filelogging"]
+    def _parse_settings(self):
+        """Override of Workflow. Calls super and collects any specificly required settings.
+        """
+        super()._parse_settings()
+        params = crf_util.get_GeneralOptions(parser=self._parser)
         self.cluster_architecture = params["cluster_architecture"]
-        self.time_span = params["time_span"]
-        self.edges = params["edges"]
-        self.no_same_neuron_edges = params["no_same_neuron_edges"]
-        self.data_file = "{}_{}.mat".format(self.experiment_group, self.condition_name)
+        params = crf_util.get_section_options(
+            "ShuffledControlsOptions", parser=self._parser, int_options=["num_shuffle"]
+        )
+        self.num_shuffle = params["num_shuffle"]
 
     def _init_settings(self):
         """Override of Workflow
         """
         super()._init_settings()
         self.shuffle_save_name = "shuffled_{}_{}".format(
-            params["experiment_name"], condition
+            self.experiment_group, self.condition_name
         )
         self.shuffle_save_dir = os.path.join(
-            params["data_directory"], "shuffled", self.experiment
+            os.path.expanduser(self.data_dir), "shuffled", self.experiment
         )
         self.shuffle_experiment = "shuffled_{}".format(self.experiment)
-
         self.shuffle_experiment_dir = os.path.join(
-            params["source_directory"], "expt", self.shuffle_experiment
+            self.source_dir, "expt", self.shuffle_experiment
         )
         # Update settings for cluster specified, if any
         if self.cluster_architecture == "yeti":
@@ -98,100 +92,97 @@ class ShuffledControlsTrial(object):
         )
         os.makedirs(os.path.expanduser(self.shuffle_experiment))
         self._start_logfile()
-
         os.makedirs(os.path.expanduser(self.shuffle_save_dir), exist_ok=True)
         # TODO: Either clear pre-existing shuffled datasets, or skip regenerating any already there
-
         self.create_shuffles()
 
+    def shuffle_training_prep(self):
+        pass
 
-def create_shuffle_configs(params, best_params):
-    fname = os.path.join(
-        params["shuffle_experiment"], "write_shuffle_configs_for_loopy.m"
-    )
-    with open(fname, "w") as f:
-        f.write("create_shuffle_configs( ...\n")
-        f.write(
-            "    'datapath', '{}.mat', ...\n".format(
-                os.path.join(params["shuffle_save_dir"], params["shuffle_save_name"])
-            )
+    def create_shuffle_configs(self, best_params):
+        fname = os.path.join(
+            self.shuffle_experiment_dir, "write_shuffle_configs_for_loopy.m"
         )
-        f.write("    'experiment_name', '{}', ...\n".format(params["shuffle_experiment"]))
-        try:
-            f.write("    'email_for_notifications', '{}', ...\n".format(params["email"]))
-        except KeyError:
-            logger.debug("No notifications email setting provided. Skipping.")
-        try:
-            f.write("    'yeti_user', '{}', ...\n".format(params["username"]))
-        except KeyError:
-            logger.debug("No yeti username provided. Skipping.")
-        f.write("    'compute_true_logZ', false, ...\n")
-        f.write("    'reweight_denominator', 'mean_degree', ...\n")
-        # f.write("    's_lambda_splits', 1, ...\n")
-        # f.write("    's_lambdas_per_split', 1, ...\n")
-        f.write("    's_lambda_min', {}, ...\n".format(best_params["s_lambda"]))
-        f.write("    's_lambda_max', {}, ...\n".format(best_params["s_lambda"]))
-        # f.write("    'density_splits', 1, ...\n")
-        # f.write("    'densities_per_split', 1, ...\n")
-        f.write("    'density_min', {}, ...\n".format(best_params["density"]))
-        f.write("    'density_max', {}, ...\n".format(best_params["density"]))
-        # f.write("    'p_lambda_splits', 1, ...\n")
-        # f.write("    'p_lambdas_per_split', 1, ...\n")
-        f.write("    'p_lambda_min', {}, ...\n".format(best_params["p_lambda"]))
-        f.write("    'p_lambda_max', {}, ...\n".format(best_params["p_lambda"]))
-        f.write("    'edges', '{}', ...\n".format(params["edges"].lower()))
-        f.write(
-            "    'no_same_neuron_edges', {}, ...\n".format(
-                str(params["no_same_neuron_edges"]).lower()
+        with open(fname, "w") as f:
+            f.write("create_shuffle_configs( ...\n")
+            f.write(
+                "    'datapath', '{}.mat', ...\n".format(
+                    os.path.join(self.shuffle_save_dir, self.shuffle_save_name)
+                )
             )
-        )
-        f.write("    'time_span', {}, ...\n".format(best_params["time_span"]))
-        f.write("    'num_shuffle', {});\n".format(params["num_shuffle"]))
-    f.closed
-    logger.info("done writing {}".format(fname))
-
-    curr_dir = os.getcwd()
-    logger.debug("curr_dir = {}.".format(curr_dir))
-    os.chdir(params["shuffle_experiment"])
-    logger.debug("changed into dir: {}".format(os.getcwd()))
-    crf_util.run_matlab_command(
-        "write_shuffle_configs_for_{},".format(MODEL_TYPE),
-        add_path=params["source_directory"],
-    )
-
-    params["shuffle_training_prep"](params)
-
-    os.chdir(curr_dir)
-    logger.debug("changed back to dir: {}".format(os.getcwd()))
-
-
-def exec_shuffle_model(source_directory, shuffle_experiment, num_shuffle, **kwargs):
-    curr_dir = os.getcwd()
-    logger.debug("curr_dir = {}.".format(curr_dir))
-    os.chdir(source_directory)
-    logger.debug("changed into dir: {}".format(os.getcwd()))
-    for cur_shuffle in range(1, num_shuffle + 1):
-        scommand = ".{}run.sh {} {}".format(os.sep, shuffle_experiment, cur_shuffle)
-        crf_util.run_command(scommand, shell=True)
-        logger.info(
-            "Trained shuffled control model {} out of {}.".format(
-                cur_shuffle, num_shuffle
+            f.write("    'experiment_name', '{}', ...\n".format(self.shuffle_experiment))
+            try:
+                f.write("    'email_for_notifications', '{}', ...\n".format(self.email))
+            except AttributeError:
+                self._logger.debug("No notifications email setting provided. Skipping.")
+            try:
+                f.write("    'yeti_user', '{}', ...\n".format(self.username))
+            except AttributeError:
+                self._logger.debug("No yeti username provided. Skipping.")
+            f.write("    'compute_true_logZ', false, ...\n")
+            f.write("    'reweight_denominator', 'mean_degree', ...\n")
+            # f.write("    's_lambda_splits', 1, ...\n")
+            # f.write("    's_lambdas_per_split', 1, ...\n")
+            f.write("    's_lambda_min', {}, ...\n".format(best_params["s_lambda"]))
+            f.write("    's_lambda_max', {}, ...\n".format(best_params["s_lambda"]))
+            # f.write("    'density_splits', 1, ...\n")
+            # f.write("    'densities_per_split', 1, ...\n")
+            f.write("    'density_min', {}, ...\n".format(best_params["density"]))
+            f.write("    'density_max', {}, ...\n".format(best_params["density"]))
+            # f.write("    'p_lambda_splits', 1, ...\n")
+            # f.write("    'p_lambdas_per_split', 1, ...\n")
+            f.write("    'p_lambda_min', {}, ...\n".format(best_params["p_lambda"]))
+            f.write("    'p_lambda_max', {}, ...\n".format(best_params["p_lambda"]))
+            f.write("    'edges', '{}', ...\n".format(self.edges.lower()))
+            f.write(
+                "    'no_same_neuron_edges', {}, ...\n".format(
+                    str(self.no_same_neuron_edges).lower()
+                )
             )
+            f.write("    'time_span', {}, ...\n".format(best_params["time_span"]))
+            f.write("    'num_shuffle', {});\n".format(self.num_shuffle))
+        f.closed
+        self._logger.info("done writing {}".format(fname))
+
+        curr_dir = os.getcwd()
+        self._logger.debug("curr_dir = {}.".format(curr_dir))
+        os.chdir(self.shuffle_experiment)
+        self._logger.debug("changed into dir: {}".format(os.getcwd()))
+        crf_util.run_matlab_command(
+            "write_shuffle_configs_for_{},".format(self.MODEL_TYPE),
+            add_path=self.source_dir,
         )
-    os.chdir(curr_dir)
-    logger.debug("changed back to dir: {}".format(os.getcwd()))
+        self.shuffle_training_prep()
+        os.chdir(curr_dir)
+        self._logger.debug("changed back to dir: {}".format(os.getcwd()))
 
+    def train_controls(self):
+        curr_dir = os.getcwd()
+        self._logger.debug("curr_dir = {}.".format(curr_dir))
+        os.chdir(self.source_dir)
+        self._logger.debug("changed into dir: {}".format(os.getcwd()))
+        for cur_shuffle in range(1, self.num_shuffle + 1):
+            scommand = ".{}run.sh {} {}".format(
+                os.sep, self.shuffle_experiment, cur_shuffle
+            )
+            crf_util.run_command(scommand, shell=True)
+            self._logger.info(
+                "Trained shuffled control model {} out of {}.".format(
+                    cur_shuffle, self.num_shuffle
+                )
+            )
+        os.chdir(curr_dir)
+        self._logger.debug("changed back to dir: {}".format(os.getcwd()))
 
-def simple_test_shuffle_datasets(
-    shuffle_save_dir, shuffle_save_name, num_shuffle, **kwargs
-):
-    filebase = os.path.join(os.path.expanduser(shuffle_save_dir), shuffle_save_name + "_")
-    return crf_util.get_max_job_done(filebase) >= num_shuffle
+    def simple_test_shuffle_datasets(self):
+        filebase = os.path.join(
+            os.path.expanduser(self.shuffle_save_dir), self.shuffle_save_name + "_"
+        )
+        return crf_util.get_max_job_done(filebase) >= self.num_shuffle
 
-
-def test_shuffle_CRFs(shuffle_experiment, num_shuffle, **kwargs):
-    filebase = os.path.join(shuffle_experiment, "results", "result")
-    return crf_util.get_max_job_done(filebase) >= num_shuffle
+    def test_shuffle_CRFs(self):
+        filebase = os.path.join(self.shuffle_experiment, "results", "result")
+        return crf_util.get_max_job_done(filebase) >= self.num_shuffle
 
     def exec_merge_shuffle_CRFs(self):
         results_path = os.path.join(self.shuffle_experiment, "results")
